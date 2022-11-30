17729dfb03bd8c85739c35db13f49ca41c581e1b
idemilson <idemilson.silva@discente.ufma.br>
2022-11-30 09:11:31 -0300

    modificando e concluindo class vm

--- src/main/java/compiladores/jackcompilador/VmWrite.java
+++ src/main/java/compiladores/jackcompilador/VmWrite.java
@@ -43,8 +43,44 @@ public class VmWrite {
     };
 
      
+    public String vmOutput(){
+        return vmOutput.toString();
+    }
      
-     
+    void writePop(Segment segment, int index){
+        vmOutput.append(String.format("pop "+segment.value + index + "\n"));
+    }
+    
+    public void writePush(Segment segment, int index) {
+        vmOutput.append (String.format ("push %s %d\n", segment.value, index));
+    } 
+    
+    public void writeArithmetic(Command command) {
+        vmOutput.append (String.format ("\n", command.name().toLowerCase()));
+    } 
 
+    public void writeLabel(String label) {
+        vmOutput.append (String.format ("label %s\n",label));
+    } 
+
+    public void writeGoto(String label) {
+        vmOutput.append (String.format ("goto %s\n",label));
+    }
+
+    public void writeIf(String label) {
+        vmOutput.append (String.format ("if-goto %s\n",label));
+    } 
+
+    public void writeCall(String name, int nArgs) {
+        vmOutput.append (String.format ("call %s %d\n", name, nArgs));
+    } 
+
+    public void writeFunction(String name, int nLocals){ 
+        vmOutput.append (String.format ("function %s %d\n", name, nLocals));
+    } 
+
+    public void writeReturn(){ 
+        vmOutput.append (String.format ("return\n"));
+    }
 
 }
